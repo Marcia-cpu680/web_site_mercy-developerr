5513cd50fc73bcf52de7f0fa0c60ec84239c9d9b
marcia galvez <marcia.galvez.rodriguez@cftmail.cl>
2024-11-08 03:34:28 -0300

    proyecto usando formularios reactivos en angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { UbicacionComponent } from './componentes/ubicacion/ubicacion.component';
 
-const routes: Routes = [];
+ const routes: Routes = [
+  
+  {
+    path:"inicio",
+    component: InicioComponent
+  },
+  {
+    path:"contacto",
+    component:ContactoComponent
+  },
+  {
+    path:"ubicacion",
+    component:UbicacionComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { UbicacionComponent } from './componentes/ubicacion/ubicacion.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    UbicacionComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
